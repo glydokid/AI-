37b1296f365ae68b58d572faddaceca6b26d7dfc
glydokid <70894372+glydokid@users.noreply.github.com>
2021-08-19 21:36:31 +0900

    K겹 활용

--- 06_K-겹.py
+++ 06_K-겹.py
@@ -6,6 +6,6 @@ digit = datasets.load_digits()
 s= svm.SVC(gamma = 0.001)
 
 for i in range(5,11):
-    accuracies = cross_val_score(s, digit.data, digit.target, cv=i) #cv= k겹 -> 5겹
+    accuracies = cross_val_score(s, digit.data, digit.target, cv=i) #cv= k겹 -> i겹
     print(accuracies)
     print("평균정확률: ", accuracies.mean()*100,"%" )
